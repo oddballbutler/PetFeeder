0fccb49ff35fb53eb1825e8c0f6f401de259e4d1
oddballbutler <oddballbutler@gmail.com>
2013-11-08 16:06:07 -0500

    Added PetFeeder.c and changed .gitignore to ignore Atmel Studio files

---
+++ PetFeeder/PetFeeder.c
@@ -0,0 +1,17 @@
+/*
+ * PetFeeder.c
+ *
+ * Created: 11/8/2013 3:52:41 PM
+ *  Author: Jeremy
+ */ 
+
+
+#include <avr/io.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
